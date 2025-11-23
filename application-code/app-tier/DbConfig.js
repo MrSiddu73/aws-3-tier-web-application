244df2e748babd10f4e035aba48a94fad71a875f
MrSiddu73 <siddusnyamagoud27@gmail.com>
2025-11-24 00:20:13 +0530

    first commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'mydatabase.cvgio4eqccbw.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Siddu1234',
+    DB_DATABASE : 'webappdb'
+});
